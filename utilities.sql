d9fa1c5d33f6f06743716cd35bf8e3d69527cffc
Bart Wiegmans <bartwiegmans@gmail.com>
2016-01-11 09:31:55 +0100

    Create setmerge function
    I'll need to figure out how to make this work generically.

---
+++ utilities.sql
@@ -0,0 +1,13 @@
+﻿drop function if exists setmerge(a int[], b int[]);
+create function setmerge(a int[], b int[]) returns int[]
+as
+$$
+begin
+	return (select array_agg(distinct e order by e) from (
+		select unnest(a || b) as e
+	) f);
+end
+$$
+language plpgsql;
+
+select setmerge(array[1,2,3],array[3,4]);
