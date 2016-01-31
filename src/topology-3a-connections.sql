d4eda8ed4af7d0d9541df95af5afc33ca93e5abb
Bart Wiegmans <bartwiegmans@gmail.com>
2016-01-31 09:41:49 +0100

    Initial query to extract high-voltage network

--- src/topology-3a-connections.sql
+++ src/topology-3a-connections.sql
@@ -1,11 +1,11 @@
 begin;
 drop table if exists problem_lines;
 drop table if exists topology_edges;
-drop table if exists heuristic_vertices;
-drop table if exists heuristic_links;
+drop table if exists topology_nodes;
+
 create table problem_lines (
     line_id varchar(64),
-    station_id text[],
+    station_id varchar(64) array,
     line_extent geometry(linestring, 3857),
     line_terminals geometry(geometry, 3857),
     station_area geometry(geometry, 3857)
@@ -13,26 +13,20 @@ create table problem_lines (
 
 create table topology_edges (
     line_id varchar(64),
-    station_id text[],
+    station_id varchar(64) array,
     line_extent geometry(linestring, 3857),
-    station_locations geometry(point, 3857) array
-);
-
-create table heuristic_vertices (
-    osm_id varchar(64),
-    osm_name    text,
-    location    geometry(point, 4326),
-    primary key (osm_id)
-);
-
-create table heuristic_links (
-    line_id varchar(64),
-    from_id varchar(64),
-    to_id   varchar(64),
-    line    geometry(linestring, 4326),
-    length  real,
+    station_locations geometry(point, 3857) array,
     primary key (line_id)
 );
+
+create table topology_nodes (
+    station_id varchar(64),
+    line_id varchar(64) array,
+    station_location geometry(point, 3857),
+    line_extents geometry(linestring, 3857) array,
+    primary key (station_id)
+);
+
 
 insert into problem_lines (line_id, station_id, line_extent, line_terminals, station_area)
     select l.osm_id as line_id, array_agg(s.osm_id) as station_id, l.extent, l.terminals, st_union(s.area)
@@ -44,12 +38,10 @@ insert into topology_edges (line_id, station_id, line_extent, station_locations)
         from power_line l join power_station s on st_intersects(s.area, l.terminals)
         group by l.osm_id, l.extent having count(*) <= 2;
 
-insert into heuristic_vertices (osm_id, location)
-    select distinct unnest(station_id), st_transform(unnest(station_locations), 4326) from topology_edges;
+insert into topology_nodes (station_id, line_id, station_location, line_extents)
+    select s.osm_id, array_agg(e.line_id), s.location, array_agg(e.line_extent)
+        from power_station s join topology_edges e on s.osm_id = any(e.station_id)
+        group by s.osm_id;
 
-insert into heuristic_links (line_id, from_id, to_id, line, length)
-    select line_id, station_id[1], station_id[2],
-           st_transform(st_makeline(station_locations[1], station_locations[2]), 4326),
-           st_length(st_transform(line_extent, 4326)::geography) from topology_edges;
 
 commit;
---
+++ src/topology-3b-high-voltage-network.sql
@@ -0,0 +1,27 @@
+begin;
+drop table if exists high_voltage_nodes;
+drop table if exists high_voltage_edges;
+
+/*
+nb - can we replace this with an update? maybe if we add a boolean high_voltage field to nodes and edges
+select unnest(station_id) from topology_edges t join electrical_properties e on t.line_id = e.osm_id where 220000 <= any(voltage) and not 16.7 = all(frequency)
+union
+select station_id from topology_nodes t join electrical_properties e on t.station_id = e.osm_id where 220000 <= any(voltage) and not 16.7 = all(frequency)
+*/
+
+with recursive high_voltage_nodes(osm_id) as (
+     /* maybe find a way to UNION with nodes-of-high-voltage-edges */
+    select n.osm_id from nodes n, electrical_properties p where p.osm_id = n.osm_id
+            and 220000 <= any(voltage) and not 16.7 = all(frequency)
+     union
+     select e.osm_id from (select unnest(nodes) from edges join high_voltage_nodes n on n.osm_id = any(nodes)) e
+            join electrical_properties p on p.osm_id = e.osm_id
+            where not 220000 > all(p.voltage) and not 16.7 = all(frequency)
+)
+insert into high_voltage_nodes (osm_id)
+       select * from high_voltage_nodes;
+insert into high_voltage_edges (line_id, left_id, right_id)
+       select line_id, left_id, right_id from edges e
+              join high_voltage_nodes a on a.osm_id = e.left_id
+              join high_voltage_nodes b on b.osm_id = e.right_id;
+commit;
