8721b33f23d402473032440e049dfa2ef1782eff
Bart Wiegmans <bartwiegmans@gmail.com>
2016-02-13 11:25:34 +0100

    Export a german-only topology

--- src/topology-5-abstraction.sql
+++ src/topology-5-abstraction.sql
@@ -4,31 +4,33 @@ drop table if exists heuristic_vertices cascade;
 drop table if exists heuristic_vertices_highvoltage;
 drop table if exists heuristic_links_highvoltage;
 
-
 -- simplify to format of scigrid export
+-- v_id,lon,lat,typ,voltage,frequency,name,operator,ref,wkt_srid_4326
 create table heuristic_vertices (
     v_id        serial primary key,
-    osm_id      varchar(64),
-    osm_objects varchar(64) array,
-    location    geometry(point, 3857),
+    lon         float,
+    lat         float,
     typ         text,
-    voltage     integer array,
-    frequency   float array,
+    voltage     text,
+    frequency   text,
     name        text,
     operator    text,
-    ref         text
+    ref         text,
+    wkt_srid_4326 text,
+    osm_id      varchar(64),
+    osm_objects varchar(64) array,
+    location    geometry(point, 3857)
 );
+
+-- l_id,v_id_1,v_id_2,voltage,cables,wires,frequency,name,operator,ref,length_m,r_ohmkm,x_ohmkm,c_nfkm,i_th_max_a,from_relation,wkt_srid_4326
 create table heuristic_links (
     l_id        serial primary key,
     v_id_1      integer references heuristic_vertices (v_id),
     v_id_2      integer references heuristic_vertices (v_id),
-    osm_id      varchar(64),
-    osm_objects varchar(64) array,
-    line        geometry(linestring, 3857),
-    voltage     integer array,
-    cables      integer array,
-    wires       integer array,
-    frequency   float array,
+    voltage     text,
+    cables      text,
+    wires       text,
+    frequency   text,
     name        text,
     operator    text,
     ref         text,
@@ -36,23 +38,52 @@ create table heuristic_links (
     r_ohmkm     float,
     x_ohmkm     float,
     c_nfkm      float,
-    i_th_max_a  float
+    i_th_max_a  float,
+    from_relation text,
+    wkt_srid_4326 text,
+    osm_id      varchar(64),
+    osm_objects varchar(64) array,
+    line        geometry(linestring, 3857)
 );
 
 create index heuristic_vertices_osm_id on heuristic_vertices (osm_id);
 create index heuristic_links_osm_id on heuristic_links (osm_id);
 
-insert into heuristic_vertices (osm_id, osm_objects, location, typ, voltage, frequency, name, operator, ref)
-    select n.station_id, o.objects, n.station_location, e.power_name, e.voltage, e.frequency, e.name, e.operator, t.tags->'ref'
+
+insert into heuristic_vertices (lon, lat, typ, voltage, frequency, name, operator, ref, wkt_srid_4326, osm_id, osm_objects, location)
+    select ST_X(ST_Transform(station_location, 4326)), 
+           ST_Y(ST_Transform(station_location, 4326)),
+           e.power_name,
+           array_to_string(e.voltage, ';'),
+           array_to_string(e.frequency, ';'),
+           e.name,
+           e.operator,
+           t.tags->'ref',
+           ST_AsEWKT(ST_Transform(station_location, 4326)),
+           station_id,
+           o.objects,
+           n.station_location
            from topology_nodes n
            join electrical_properties e on e.osm_id = n.station_id
            join osm_objects o on o.osm_id = n.station_id
            join osm_tags t on t.osm_id = n.station_id;
 
 
-insert into heuristic_links (v_id_1, v_id_2, osm_id, osm_objects, line, length_m, voltage, cables, wires, frequency, name, operator, ref)
-    select a.v_id, b.v_id, l.line_id, o.objects, st_makeline(a.location, b.location), st_length(st_transform(l.line_extent, 4326)::geography),
-           e.voltage, e.conductor_bundles, e.subconductors, e.frequency, e.name, e.operator, t.tags->'ref'
+insert into heuristic_links (v_id_1, v_id_2, length_m, voltage, cables, wires, frequency, name, operator, ref, from_relation, wkt_srid_4326, osm_id, osm_objects, line)
+    select a.v_id,
+           b.v_id,
+           st_length(st_transform(l.line_extent, 4326)::geography),
+           array_to_string(e.voltage, ';'),
+           array_to_string(e.conductor_bundles, ';'),
+           array_to_string(e.subconductors, ';'),
+           array_to_string(e.frequency, ';'),
+           e.name,
+           e.operator,
+           t.tags->'ref', '',
+           ST_AsEWKT(ST_Transform(ST_MakeLine(a.location, b.location), 4326)),
+           l.line_id,
+           o.objects,
+           st_makeline(a.location, b.location)
            from topology_edges l
            join electrical_properties e on e.osm_id = l.line_id
            join osm_objects o on o.osm_id = l.line_id
--- util/load_polyfile.py
+++ util/load_polyfile.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 from __future__ import print_function, unicode_literals, division
+import argparse
 import sys
 import os
 import io
 from polyfile import PolyfileParser
 from geometry import Polygon
 
+ap = argparse.ArgumentParser()
+ap.add_argument('file', nargs='+', type=str)
+ap.add_argument('--table', type=str, default='polygons')
+args = ap.parse_args()
+
 polygons = dict()
 parser = PolyfileParser()
-for file_name in sys.argv[1:]:
+for file_name in args.file:
     if not os.path.isfile(file_name):
         print("Usage: {0} <files>".format(sys.argv[0]))
     name, ext = os.path.splitext(os.path.basename(file_name))
@@ -17,17 +23,20 @@ for file_name in sys.argv[1:]:
         pl = Polygon(pr[1]['1'])
         polygons[name] = pl.to_wkt()
     except Exception as e:
-        print("Could not process {0} because {1}".format(file_name, e))
+        print("Could not process {0} because {1}".format(file_name, e), file=sys.stderr)
         quit(1)
 
+values = ','.join("('{0}', ST_SetSRID(ST_GeomFromText('{1}'), 4326))".format(n, p)
+                  for (n, p) in polygons.items())
 
-
-with io.open('import-polygons.sql', 'w') as handle:
-    handle.write('''
-CREATE TABLE IF NOT EXISTS polygons (
+print('''
+BEGIN;
+DROP TABLE IF EXISTS {0};
+CREATE TABLE {0} (
     name varchar(64) primary key,
     polygon geometry(polygon, 4326)
 );
-INSERT INTO polygons (name, polygon) VALUES {0};
-'''.format(','.join("('{0}', ST_SetSRID(ST_GeomFromText('{1}'), 4326))".format(n, p) for (n, p) in polygons.items())))
+INSERT INTO {0} (name, polygon) VALUES {1};
+COMMIT;
+'''.format(args.table, values))
 # of course you can abuse this. don't do that, then
