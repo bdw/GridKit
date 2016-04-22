580a2186a41e87710ed9c3347739aeeb683ad796
Bart Wiegmans <bartwiegmans@gmail.com>
2016-04-22 22:35:20 +0200

    Change osm_name to source_id

--- src/electric-3-compute.sql
+++ src/electric-3-compute.sql
@@ -28,6 +28,7 @@ create table computed_structure (
 
 create function electric_structure_from_object(o jsonb) returns electric_structure array as $$
 begin
+--    raise notice 'electric structure from object %', o::text;
     return case when o ? 'source' then electric_structure_from_tags(o->>'source')
                 when o ? 'merge' then electric_structure_from_lateral_merge(o)
                 when o ? 'join'  then electric_structure_from_end_join(o)
@@ -42,20 +43,28 @@ end;
 $$ language plpgsql;
 
 create function electric_structure_from_end_join(j jsonb) returns electric_structure array as $$
+declare
+
 begin
-    return null;
+    raise notice 'electric structure from end join on %', j::text;
+    raw = array(select unnest(electric_structure_from_object(o)) from jsonb_array_elements(j->'join') a(o));
+    grp = array(select row((e).voltage, (e).frequency, (e).cables, (e).wires, sum((e).num_objects), sum((e).num_conflicts))
+                       from (select unnest(raw)) t(e)
+                       group by (e).voltage, (e).frequency, (e).cables, (e).wires);
+    return grp;
 end;
 $$ language plpgsql;
 
 create function electric_structure_from_tags(o text) returns electric_structure array
 as $$
 begin
+     raise notice 'electric structure from tags on %', o;
      return array(select row(v, f, c, w, 1, 0) from (
           select case when voltage is not null then unnest(voltage) end,
                  case when frequency is not null then unnest(frequency) end,
                  case when cables is not null then unnest(cables) end,
                  case when wires is not null then unnest(wires) end
-                 from electric_tags where osm_name = o
+                 from electric_tags where source_id = o
           ) e (v, f, c, w)
      );
 end;
