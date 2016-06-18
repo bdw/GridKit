bf4889e9c5a2b87f9095238163c2dfb1f2784213
Bart Wiegmans <bartwiegmans@gmail.com>
2016-06-18 11:38:01 +0200

    Complete the README for the entso-e dataset

--- entsoe/export.sh
+++ entsoe/export.sh
@@ -4,4 +4,4 @@ psql -c "COPY network_link TO STDOUT WITH CSV HEADER QUOTE ''''" > links.csv
 psql -c "COPY network_generator TO STDOUT WITH CSV HEADER QUOTE ''''" > generators.csv
 psql -c "COPY network_transformer TO STDOUT WITH CSV HEADER QUOTE ''''" > transformers.csv
 
-zip entsoe.zip buses.csv links.csv generators.csv transformers.csv
+zip entsoe.zip README.md buses.csv links.csv generators.csv transformers.csv
