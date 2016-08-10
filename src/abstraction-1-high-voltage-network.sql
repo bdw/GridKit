eecb06d1ca790a948646b16461bea12db07864ef
Bart Wiegmans <bartwiegmans@gmail.com>
2016-08-10 16:27:58 +0200

    Simpler, faster high-voltage network extraction
    Recursive query was slow and gave counterintuitive results

--- src/abstraction-1-high-voltage-network.sql
+++ src/abstraction-1-high-voltage-network.sql
@@ -1,51 +1,56 @@
 begin;
-drop table if exists high_voltage_nodes cascade;
-drop table if exists high_voltage_edges cascade;
-
-create table high_voltage_nodes (
-    station_id integer primary key,
-    station_location geometry(point, 3857),
-    topology_name varchar(64)
-);
-
-create table high_voltage_edges (
+drop table if exists high_voltage_lines;
+drop table if exists high_voltage_stations;
+create table high_voltage_lines (
     line_id integer primary key,
-    station_id integer array,
-    line_extent geometry(linestring, 3857),
+    line_extent geometry(linestring,3857) -- for visualization
+);
+create table high_voltage_stations (
+    station_id integer primary key,
+    station_location geometry(point, 3857)
 );
 
-with recursive high_voltage_stations (station_id) as (
-    select n.station_id from topology_nodes n
-        join electrical_properties p on p.power_id = n.station_id and p.power_type = 's'
-        where 220000 <= any(p.voltage) and (not 16.7 = all(p.frequency) or p.frequency is null)
+-- because we've added the line voltage to the station structure in
+-- the previous step, we don't need to check lines to find nodes anymore
+insert into high_voltage_stations (station_id, station_location)
+     select n.station_id, n.station_location
+       from topology_nodes n
+       join station_structure s on n.station_id = s.station_id
+      where 220000 <= any(s.voltage)
+        and (not 16.7 = all(s.frequency) or array_length(s.frequency,1) is null);
 
-        union
 
-   select unnest(e.station_id) from topology_edges e
-        join electrical_properties p on p.power_id = e.line_id and p.power_type = 'l'
-        where 220000 <= any(p.voltage) and (not 16.7 = all(p.frequency) or p.frequency is null)
-
-        union
-
-   select station_id from (
-       select line_id, unnest(e.station_id) from topology_edges e
-           join high_voltage_stations h on array[h.station_id] <@ e.station_id
-   ) f (line_id, station_id)
-      join electrical_properties l on l.power_id = f.line_id    and l.power_type = 's'
-      join electrical_properties s on s.power_id = f.station_id and s.power_type = 'l'
-       where (not 220000 > all(l.voltage) or l.voltage is null)
-         and (not 16.7 = all(l.frequency) or l.frequency is null)
-         and (not 220000 > all(s.voltage) or s.voltage is null)
-         and (not 16.7 = all(s.frequency) or s.frequency is null)
-)
-insert into high_voltage_nodes (station_id, station_location, topology_name)
-   select h.station_id, n.station_location, n.topology_name
-       from high_voltage_stations h
-       join topology_nodes n on n.station_id = h.station_id;
-
-insert into high_voltage_edges (line_id, station_id, line_extent, direct_line)
-   select e.line_id, e.station_id, e.line_extent, e.direct_line
+-- we do need to repeat the station check, otherwise it isn't
+-- sensitive enough (NULLs are ok, exclusion values are not)
+insert into high_voltage_lines (line_id, line_extent)
+     select e.line_id, line_extent
        from topology_edges e
-       join high_voltage_nodes a on a.station_id = e.station_id[1]
-       join high_voltage_nodes b on b.station_id = e.station_id[2];
+       join line_structure l on l.line_id = e.line_id
+      where l.voltage >= 220000 and (l.frequency != 16.7 or l.frequency is null)
+
+      union
+
+     select e.line_id, line_extent
+       from topology_edges e
+       join station_structure a on a.station_id = e.station_id[1]
+       join station_structure b on b.station_id = e.station_id[2]
+      where (not 220000 >= all(a.voltage) or array_length(a.voltage, 1) is null)
+        and (not 220000 >= all(b.voltage) or array_length(b.voltage, 1) is null)
+        and (not 16.7 = all(a.frequency) or array_length(a.frequency, 1) is null)
+        and (not 16.7 = all(b.frequency) or array_length(b.frequency, 1) is null)
+        and ( 220000 <= any(a.voltage) or 220000 <= any(b.voltage) );
+
+-- but we can have added lines to stations not in the high-voltage
+-- set, and that is not good
+insert into high_voltage_stations (station_id, station_location)
+     select station_id, station_location
+       from topology_nodes n
+      where exists (
+            select 1 from high_voltage_lines h
+             where h.line_id = any(n.line_id)
+          )
+        and not exists (
+            select 1 from high_voltage_stations h
+             where h.station_id = n.station_id
+          );
 commit;
