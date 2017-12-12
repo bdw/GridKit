8df226b71d5a1a8c786d176719a38e5970c054bc
Jonas Hoersch <jonas@chaoflow.net>
2017-12-12 13:57:23 +0100

    entsoe: Export all new tables as csv files

--- entsoe/export.sh
+++ entsoe/export.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 psql -c "COPY network_bus TO STDOUT WITH CSV HEADER QUOTE ''''" > buses.csv
+psql -c "COPY network_line TO STDOUT WITH CSV HEADER QUOTE ''''" > lines.csv
 psql -c "COPY network_link TO STDOUT WITH CSV HEADER QUOTE ''''" > links.csv
-psql -c "COPY network_generator TO STDOUT WITH CSV HEADER QUOTE ''''" > generators.csv
+psql -c "COPY network_converter TO STDOUT WITH CSV HEADER QUOTE ''''" > converters.csv
 psql -c "COPY network_transformer TO STDOUT WITH CSV HEADER QUOTE ''''" > transformers.csv
+# psql -c "COPY network_generator TO STDOUT WITH CSV HEADER QUOTE ''''" > generators.csv
 
-zip entsoe.zip README.md buses.csv links.csv generators.csv transformers.csv
+zip entsoe.zip README.md buses.csv lines.csv links.csv converters.csv transformers.csv generators.csv
