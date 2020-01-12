f64e65ab36b4e4345d7b41fcc708c8a0f54018e7
Jonas Hörsch <jonas.hoersch@mobilityhouse.com>
2020-01-12 22:46:51 +0100

    entsoe: Move data files to separate directories

--- entsoe/import.sh
+++ entsoe/import.sh
@@ -3,5 +3,5 @@
 BASE=${1-entsoe}
 ZOOM=${2-6}
 
-python fixup-mapbox-tiles.py ${BASE} ${BASE}*-z${ZOOM}.geojson
-python ../util/geojson-to-postgis.py ${BASE}-*-fixed.geojson
+python fixup-mapbox-tiles.py data/${BASE} data/${BASE}*-z${ZOOM}.geojson
+python ../util/geojson-to-postgis.py data/${BASE}-*-fixed.geojson
