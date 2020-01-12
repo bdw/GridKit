839592855d887661c633015f31d2565935a43baa
Jonas Hörsch <jonas.hoersch@mobilityhouse.com>
2020-01-12 22:42:09 +0100

    entsoe: remove old mapbox handles

--- entsoe/download.sh
+++ entsoe/download.sh
@@ -5,7 +5,7 @@ ZOOM=${1-6}
 npm install vt-geojson
 
 export MapboxAccessToken=pk.eyJ1IjoiZW50c29lIiwiYSI6ImNpbWxxYXJocDAwMG53Ymx3N2JxNGhtZDYifQ.YjNgK9usqRNrzxWXnR152g
-for key in rusty.cm0b8gzp rusty.02rit83j entsoe.1nr7sorj entsoe.5m43hs6u; do
+for key in entsoe.1nr7sorj entsoe.5m43hs6u; do
     echo Getting ${key}
-    node_modules/vt-geojson/cli.js ${key} -z ${ZOOM} > ${key}-z${ZOOM}.geojson
+    node_modules/vt-geojson/cli.js ${key} -z ${ZOOM} > data/${key}-z${ZOOM}.geojson
 done
